df6a6a36134c6bb0dd2236977c3811399e7d999c
Tomas Kelečius <sleepwell95@gmail.com>
2020-01-23 14:04:48 +0200

    First and Last Commit. For usage in CLASS!!!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Hi, I'm a React App</h1>
+       <p>This is really working!</p>
+       <Person name="Timas" age="28" />
+       <Person name="Manu" age="29" >My Hobbies: Racing</Person>
+       <Person name="Stephanie" age="26" />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
 export default App;
